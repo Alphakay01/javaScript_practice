320cd8ec462348731c121f994056981464ee7620
olatilewa kamaldeen <olatilewkamal@yahoo.com>
2023-10-11 17:51:45 -0500

    my javaScript practice

---
+++ script.js
@@ -0,0 +1,532 @@
+// let str_1 = "We can write a script to automate stuffs"
+// let str_2 = "Progamming langs include: 'Java', 'Python', 'Javascript', and 'Go' "
+// let str_3 = "We'll all have a swell weekend of coding"
+
+
+// if (str_1.includes("script")) {
+//     console.log("Yes, I can find 'script'")
+// }
+
+// else {
+//     console.log("No! Cant't find script")
+// }
+
+
+// if (str_2.includes("script")) {
+//     console.log("Yes, I can find 'script'")
+// }
+
+// else {
+//     console.log("No! Cant't find script")
+// }
+
+
+// if (str_3.includes("script")) {
+//     console.log("Yes, I can find 'script'")
+// }
+
+// else {
+//     console.log("No! Cant't find script")
+// }
+
+// 2.
+// let line_4 = "Write a program to check if this string is big string is big"
+// let line_5 = " given a string 'name'. "
+// let line_6 = "JS"
+
+
+
+// if (line_4.length < 10) {
+//     console.log("True")
+// }
+
+// else {
+//     console.log("False")
+// }
+
+// if (line_5.length < 10) {
+//     console.log("True")
+// }
+
+// else {
+//     console.log("False")
+// }
+
+// if (line_6.length < 10) {
+//     console.log("True")
+// }
+
+// else {
+//     console.log("False")
+// }
+
+// let inputData
+// inputData = "Enter Your Message"
+
+// initialCasing = inputData.charAt(0).toUpperCase() + inputData.replace(inputData.charAt(0), '')
+// console.log(initialCasing)
+
+// let inputDat;
+// inputDat = "Enter Your Message"
+// initialCase = inputDat.replace(inputDat.charAt(0).toLowerCase, inputDat.charAt(0).toUpperCase)
+// console.log(initialCase)
+
+// let encode;
+// encode= "Input The Characters You Want To Encode"
+// let  trimmerfxn = encode.trim()
+// let codedHacker = trimmerfxn.replace(/i/g, "1")
+//     .replace(/o/g, "0") 
+//     .replace(/a/g, "4")
+//     .replace(/s/g, "5") 
+//     .replace(/i/g, "1")
+//     .replace(/e/g, "3")
+//     .replace(/A/g, "4") 
+//     .replace(/z/g, "2") 
+  
+//     console.log(codedHacker)
+
+//     // console.log(inputData.replace(/""||/g, "z"))
+
+// const stringWords = inputData.split(' ')
+
+// if (inputDat.length = inputData.length) {
+//     console.log("True")
+// }
+// else {
+//     console.log("False")
+// }
+
+// let inputDat = "sleep"
+//  inputData = "people"
+// if (inputDat.includes(inputData)) {
+//     console.log(containerword && contained)
+// }
+// else {
+//     console.log('""')
+// };
+
+//     let line_1 = "flower"
+//     let line_2 = "flow"
+//     let line_3 = "flowering"
+//     let input = ""
+//    if (line_1[0] === line_2[0] && line_2[0]=== line_3[0]) {
+//     input = input + line_1[0]
+//    }
+//    if (line_1[1] === line_2[1] && line_2[1]=== line_3[1] && input === line_1[0]) {
+//     input = input + line_1[1]
+//    }
+//    if (line_1[2] === line_2[2] && line_2[2]=== line_3[2] && input === line_1[0] + line_1[1]) {
+//     input = input + line_1[2]
+//    }
+//    if (line_1[3] === line_2[3] && line_2[3]=== line_3[3] && input === line_1[0] + line_1[1] +  line_1[2]) {
+//     input = input + line_1[3]
+//    }
+//    console.log(input);
+
+// function multiply(num1, num2,) {
+//     let product = num1 * num2
+//     return product
+// }
+
+
+// function divide(num1, num2) {
+//     let division = num1 / num2
+//     return division
+// }
+// console.log(multiply(7, 4))
+// console.log(multiply(2, 4))
+// console.log(multiply(10, 5))
+// console.log(divide(2, 4))
+// console.log(divide(10, 5))
+// console.log(divide(7, 9))
+
+// function subtract(num1, num2){
+// if (num1 >= num2)
+// {let difference = num1 - num2
+//     return difference = num1 - num2
+// }
+// else { return difference = num2 - num1
+
+// }
+// }
+    
+// function wordsieve(str1, str2, str3){
+//     if (str1[0] == str2[0] && str1[0] == str3[0] && str2[0] == str3[0]) {
+//         let=output_ = output_ + str1[0]
+//         }
+//         else output_ = output_
+//     if (output_ == str1[0] && str1[1] == str2[1] && str1[1] == str3[1] && str2[1] == str3[1]) {
+//         output_ = output_ + str1[1]
+//         }
+//         else output = output
+//     if (output_ == str1[0] + str1[1] && str1[2] == str2[2] && str1[2] == str3[2] && str2[2] == str3[2]) {
+//         output_ = output_ + str1[2]
+//         }
+//         else output_ = output_
+//     if (output == str1[0] + str1[1] + str1[2] && str1[3] == str2[3] && str1[3] == str3[3] && str2[3] == str3[3]) {
+//         output_ = output_ + str1[3]
+//         }
+//         else output = output
+//     console.log(output_)
+//         }
+    // let  num = "CCXCV"
+    // let output = 0
+    
+    // //charcter1
+    // if (num[0] =="C") {
+    //     output = output + 100
+    //     }
+    //     else if(num[0] == "L") {
+    //         output = output + 50
+    //     }
+    //     else if(num[0] == "X") {
+    //         output = output + 10
+    //     }
+    //     else if(num[0] == "V") {
+    //         output = output + 5
+    //     }
+    //     else if(num[0] == "I") {
+    //         output = output + 1
+    //     }
+    
+    
+    // //character2
+    // if (num[1] == "C" && num[0] != "X") {
+    //     output = output + 100
+    //     }
+    //     else if (num[1] == "C" && num[0] == "X") {
+    //         output = output + 80
+    //     }
+    //     else if (num[1] == "L" && num[0] != "X") {
+    //         output = output + 50
+    //     }
+    //     else if (num[1] == "L" && num[0] == "X") {
+    //         output = output + 30
+    //     }
+    //     else if (num[1] == "X" && num[0] != "I") {
+    //         output = output + 10
+    //     }
+    //     else if (num[1] == "X" && num[0] == "I") {
+    //         output = output + 8
+    //     }
+    //     else if (num[1] == "V" && num[0] != "I") {
+    //         output = output + 5
+    //     }
+    //     else if (num[1] == "V" && num[0] == "I") {
+    //         output = output + 3
+    //     }
+    //     else if (num[1] == "I") {
+    //         output = output + 1
+    //     }
+    
+    
+    // //character3
+    // if (num[2] == "C" && num[1] != "X") {
+    //     output = output + 100
+    //     }
+    //     else if (num[2] == "C" && num[1] == "X") {
+    //         output = output + 80
+    //     }
+    //     else if (num[2] == "L" && num[1] != "X") {
+    //         output = output + 50
+    //     }
+    //     else if (num[2] == "L" && num[1] == "X") {
+    //         output = output + 30
+    //     }
+    //     else if (num[2] == "X" && num[1] != "I") {
+    //         output = output + 10
+    //     }
+    //     else if (num[2] == "X" && num[1] == "I") {
+    //         output = output + 8
+    //     }
+    //     else if (num[2] == "V" && num[1] != "I") {
+    //         output = output + 5
+    //     }
+    //     else if (num[2] == "V" && num[1] == "I") {
+    //         output = output + 3
+    //     }
+    //     else if (num[2] == "I") {
+    //         output = output + 1
+    //     }
+    
+    
+    // //character4
+    // if (num[3] == "C" && num[2] != "X") {
+    //     output = output + 100
+    //     }
+    //     else if (num[3] == "C" && num[2] == "X") {
+    //         output = output + 80
+    //     }
+    //     else if (num[3] == "L" && num[2] != "X") {
+    //         output = output + 50
+    //     }
+    //     else if (num[3] == "L" && num[2] == "X") {
+    //         output = output + 30
+    //     }
+    //     else if (num[3] == "X" && num[2] != "I") {
+    //         output = output + 10
+    //     }
+    //     else if (num[3] == "X" && num[2] == "I") {
+    //         output = output + 8
+    //     }
+    //     else if (num[3] == "V" && num[2] != "I") {
+    //         output = output + 5
+    //     }
+    //     else if (num[3] == "V" && num[2] == "I") {
+    //         output = output + 3
+    //     }
+    //     else if (num[3] == "I") {
+    //         output = output + 1
+    //     }
+    
+    
+    // //character5
+    // if (num[4] == "C" && num[3] != "X") {
+    //     output = output + 100
+    //     }
+    //     else if (num[4] == "C" && num[3] == "X") {
+    //         output = output + 80
+    //     }
+    //     else if (num[4] == "L" && num[3] != "X") {
+    //         output = output + 50
+    //     }
+    //     else if (num[4] == "L" && num[3] == "X") {
+    //         output = output + 30
+    //     }
+    //     else if (num[4] == "X" && num[3] != "I") {
+    //         output = output + 10
+    //     }
+    //     else if (num[4] == "X" && num[3] == "I") {
+    //         output = output + 8
+    //     }
+    //     else if (num[4] == "V" && num[3] != "I") {
+    //         output = output + 5
+    //     }
+    //     else if (num[4] == "V" && num[3] == "I") {
+    //         output = output + 3
+    //     }
+    //     else if (num[4] == "I") {
+    //         output = output + 1
+    //     }
+    
+    // console.log(output)
+    
+    // 5. Write a JavaScript program that capitalizes the first letter of each word of a given string.
+    
+    // Example
+    // For "the quick brown fox"; Print "The Quick Brown Fox"
+    // For "java script"; Print "Java Script"
+    
+    // let sCase= "frank is tough buddy"
+    // let firstOcc = sCase.indexOf(" ")
+    // let subStr1 = sCase[0].toUpperCase() + sCase.slice(1, firstOcc)
+    // let secondOcc = sCase.indexOf(" ", firstOcc + 1)
+    // let thirdOcc = sCase.indexOf(" ", secondOcc + 1)
+    // let subStr2 = sCase.charAt(firstOcc + 1).toUpperCase() + sCase.slice(firstOcc + 2, secondOcc )
+    // let subStr3 = sCase.charAt(secondOcc + 1).toUpperCase() + sCase.slice(secondOcc + 2, thirdOcc)
+    // let subStr4 = sCase.charAt(thirdOcc + 1).toUpperCase() + sCase.slice(thirdOcc + 2)
+    
+    
+    // let uCase = subStr1 + " " + subStr2  + " " + subStr3 + " " + subStr4
+    // console.log(uCase)
+    
+    // function capitalizer(sCase){
+    //     let firstOcc = sCase.indexOf(" ")
+    //     let subStr1 = sCase[0].toUpperCase() + sCase.slice(1, firstOcc)
+    //     let secondOcc = sCase.indexOf(" ", firstOcc + 1)
+    //     let thirdOcc = sCase.indexOf(" ", secondOcc + 1)
+    //     let subStr2 = sCase.charAt(firstOcc + 1).toUpperCase() + sCase.slice(firstOcc + 2, secondOcc )
+    //     let subStr3 = sCase.charAt(secondOcc + 1).toUpperCase() + sCase.slice(secondOcc + 2, thirdOcc)
+    //     let subStr4 = sCase.charAt(thirdOcc + 1).toUpperCase() + sCase.slice(thirdOcc + 2)
+        
+        
+    //     let uCase = subStr1 + " " + subStr2  + " " + subStr3 + " " + subStr4
+    //     console.log(uCase)
+    //     return uCase 
+    //     }
+    //     console.log(capitalizer("monday will be good"))
+    
+    
+    //   number!
+       
+       
+    //  function capiterlizer(encode){
+    // let  trimmerfxn = encode.trim()
+    // let codedHacker = trimmerfxn.replaceAll("i", "1")
+    //     .replaceAll("o", "0") 
+    //     .replaceAll("a", "4")
+    //     .replaceAll("s", "5") 
+    //     .replaceAll("i", "1")
+    //     .replaceAll("e", "3")
+      
+    //     return(codedHacker)
+    // }
+       
+    //   console.log(capiterlizer("    Alhamdulillahi Robbilalamin"))
+       
+    // // //   Nom2
+    //     let output = ""
+        
+    //     function lettersieve(line_1, line_2, line_3){
+    //     if (line_1[0] == line_2[0] && line_2[0] == line_3[0]) {
+    //     output = output + line_1[0]
+    //     }
+    //     else {output= output}
+    //     if (line_1[1] == line_2[1] && line_2[1] == line_3[1] && output == line_1[0]) {
+    //     output = output + line_1[1]
+    //     }
+    //     else {output= output}
+    //     if (line_1[2] === line_2[2] && line_2[2] == line_3[2] && output == line_1[0] + line_1[1]) {
+    //     output = output + line_1[2]
+    //     }
+    //     else {output= output}
+    //     if (line_1[3] === line_2[3] && line_2[3] == line_3[3] && output == line_1[0] + line_1[1] +  line_1[2]) {
+    //     output = output + line_1[3]
+    //     }
+    //     else {output= output}
+    //     return (output);  
+    //     }
+        
+        
+    //       console.log(lettersieve("0lower", "fow", "flowt"))
+        
+    //     // nom3
+    // let output = ""
+    
+    // function romannumerals(num){
+    // if (num[0] =="C") {
+    //     output = output + 100
+    //     }
+    //     else if(num[0] == "L") {
+    //         output = output + 50
+    //     }
+    //     else if(num[0] == "X") {
+    //         output = output + 10
+    //     }
+    //     else if(num[0] == "V") {
+    //         output = output + 5
+    //     }
+    //     else if(num[0] == "I") {
+    //         output = output + 1
+    //     }
+    
+    
+    // //character2
+    // if (num[1] == "C" && num[0] != "X" && num[0] !="L" && num[0] !="V" && num[0] !="I" ) {
+    //     output = output + 100
+    //     }
+    //     else if (num[1] == "C" && num[0] == "X") {
+    //         output = output + 80
+    //     }
+    //     else if (num[1] == "L" && num[0] != "X") {
+    //         output = output + 50
+    //     }
+    //     else if (num[1] == "L" && num[0] == "X") {
+    //         output = output + 30
+    //     }
+    //     else if (num[1] == "X" && num[0] != "I") {
+    //         output = output + 10
+    //     }
+    //     else if (num[1] == "X" && num[0] == "I") {
+    //         output = output + 8
+    //     }
+    //     else if (num[1] == "V" && num[0] != "I") {
+    //         output = output + 5
+    //     }
+    //     else if (num[1] == "V" && num[0] == "I") {
+    //         output = output + 3
+    //     }
+    //     else if (num[1] == "I") {
+    //         output = output + 1
+    //     }
+    
+    
+    // //character3
+    // if (num[2] == "C" && num[1] != "X") {
+    //     output = output + 100
+    //     }
+    //     else if (num[2] == "C" && num[1] == "X") {
+    //         output = output + 80
+    //     }
+    //     else if (num[2] == "L" && num[1] != "X") {
+    //         output = output + 50
+    //     }
+    //     else if (num[2] == "L" && num[1] == "X") {
+    //         output = output + 30
+    //     }
+    //     else if (num[2] == "X" && num[1] != "I") {
+    //         output = output + 10
+    //     }
+    //     else if (num[2] == "X" && num[1] == "I") {
+    //         output = output + 8
+    //     }
+    //     else if (num[2] == "V" && num[1] != "I") {
+    //         output = output + 5
+    //     }
+    //     else if (num[2] == "V" && num[1] == "I") {
+    //         output = output + 3
+    //     }
+    //     else if (num[2] == "I") {
+    //         output = output + 1
+    //     }
+    
+    //     return output
+        
+    // }
+        
+    //     console.log(romannumerals("IC"))
+        
+        
+    
+    // function blockingFirstCase(sCase) {
+    // let firstOcc = sCase.indexOf(" ")
+    // let subStr1 = sCase[0].toUpperCase() + sCase.slice(1, firstOcc)
+    // let secondOcc = sCase.indexOf(" ", firstOcc + 1)
+    // let thirdOcc = sCase.indexOf(" ", secondOcc + 1)
+    // let subStr2 = sCase.charAt(firstOcc + 1).toUpperCase() + sCase.slice(firstOcc + 2, secondOcc )
+    // let subStr3 = sCase.charAt(secondOcc + 1).toUpperCase() + sCase.slice(secondOcc + 2, thirdOcc)
+    // let subStr4 = sCase.charAt(thirdOcc + 1).toUpperCase() + sCase.slice(thirdOcc + 2)
+    
+    
+    // let uCase = subStr1 + " " + subStr2  + " " + subStr3 + " " + subStr4
+    //     return uCase
+    //     }
+        
+    //     console.log(blockingFirstCase("Frank is agfg buuddy"))
+    
+    
+    
+    // // nom4
+    // function worddepth(str1, str2){
+    //     if (str1.length === str2.length){
+    //         return "True"
+    //     }
+    //     else {return "False"}
+    // }
+    // console.log( worddepth("strghdvjh1", "shhjhtr2"))
+    
+    
+    
+    // str2.replace(str2[i+1], str2[i+1].toUpperCase()
+    
+    
+    //     function callLetter(input){
+    //         for (i=0; i<input.length; i++){
+    //             output = input[i]
+    //             console.log(output)
+            
+    //     }
+    //             return output
+    //     }
+    //     console.log(callLetter("Alhamdulillahi"))
+         
+         /*
+        algorithm
+        step1 compare the length of the two given strings s and t, if the lengths
+        are equal proceed to 2. otherwise return false.
+        step2 map using LOOP the characters in string s with the characters in other string
+        and SLICE if it exists and RETURN true otherwise RETURN false.
+        
+        */
+        
+        
